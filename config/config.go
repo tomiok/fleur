4e2f5b7f26824b729d07fc69b6bc25e87ea345fb
Tomas Lingotti <tomaslingotti@gmail.com>
2021-10-14 16:01:32 -0300

    print configs

--- config/config.go
+++ config/config.go
@@ -1,17 +1,42 @@
 package config
 
-import "os"
+import (
+	"encoding/json"
+	"github.com/rs/zerolog/log"
+	"os"
+)
+
+var c *Config
 
 type Config struct {
-	Port string
+	Port string `json:"port"`
 }
 
 func Bind() *Config {
+	if c != nil {
+		return c
+	}
+
 	var port = os.Getenv("PORT")
 
 	if port == "" {
 		port = "5566"
 	}
 
-	return &Config{Port: port}
+	c = &Config{Port: port}
+	return c
+}
+
+func (conf *Config) PrintConfigs() {
+	if conf == nil {
+		log.Print("configs are nil")
+		return
+	}
+	b, err := json.MarshalIndent(conf, "", "    ")
+
+	if err != nil {
+		log.Printf("cannot marshal configs %s", err.Error())
+	}
+
+	log.Print(string(b))
 }
--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"github.com/ohchat-io/fleur/config"
 	"github.com/ohchat-io/fleur/srv"
 )
+
 //TODO fix broadcasting, do a correct one
 //TODO add init functions for TCP and chat servers
 func main() {
 	conf := config.Bind()
+	conf.PrintConfigs()
 	s := srv.NewServer(conf.Port)
 	cs := srv.ChatServer{
 		ActiveConnections: make(map[string]*srv.Conn),
